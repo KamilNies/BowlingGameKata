7ff7c1bd03cd597f1e3065e3d36575ab5ef3dff2
Kamil Niescieronek <kamil.niescieronek@iths.se>
2021-12-22 12:40:24 +0100

    A_game_with_1_spare_and_1_strike_at_frame_10 test added. Still works

--- BowlingGameTest/UnitTest1.cs
+++ BowlingGameTest/UnitTest1.cs
@@ -244,5 +244,23 @@ namespace BowlingGameTest
 
             Assert.Equal(84, game.Score());
         }
+
+        [Fact]
+        public void A_game_with_1_spare_and_1_strike_at_frame_10()
+        {
+            var game = new Game();
+            var rolls = new int[21]
+            {
+                4, 6, 3, 6, 4, 2, 8, 1, 0, 2, 2,
+                0, 2, 3, 0, 3, 4, 3, 5, 5, 10
+            };
+
+            foreach (var roll in rolls)
+            {
+                game.Roll(roll);
+            }
+
+            Assert.Equal(76, game.Score());
+        }
     }
 }
